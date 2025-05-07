1ad07c1cc3917eaf1c694122a0c88531bf23ce6d
Þórður Hafliðason <tolleinn@gmail.com>
2025-05-07 21:06:12 +0000

    Fix draft state

--- libs/application/templates/tax-return/src/screens/SecondInterestCharges.tsx
+++ libs/application/templates/tax-return/src/screens/SecondInterestCharges.tsx
@@ -2,39 +2,11 @@ import { useLocale } from '@island.is/localization'
 import { FormScreen } from '../components/form/FormScreen'
 import { taxSecondInterestCharges } from '../lib/messages'
 import { OJOIFieldBaseProps } from '../lib/types'
-import {
-  formatCurrencyWithoutSuffix,
-  formatPhoneNumber,
-} from '@island.is/application/ui-components'
-import { Box, Button, Stack, Text } from '@island.is/island-ui/core'
-import { DisplayTable } from '../components/DisplayTable/DisplayTable'
-import { FormChapter } from '../components/FormChapter/FormChapter'
-import { Body, Data, Row, Table } from 'libs/island-ui/core/src/lib/Table/Table'
-import { Routes } from '../lib/constants'
 
 export const SecondInterestChargesScreen = ({
-  application,
   goToScreen,
 }: OJOIFieldBaseProps) => {
   const { formatMessage: f } = useLocale()
-
-  const chapterIncomeMap = [
-    taxSecondInterestCharges.salarySubChapter,
-    taxSecondInterestCharges.grantsSubChapter,
-    taxSecondInterestCharges.otherRevenueSubChapter,
-  ]
-
-  const chapterPropertyMap = [
-    taxSecondInterestCharges.localPropertySubChapter,
-    taxSecondInterestCharges.vehiclesSubChapter,
-  ]
-
-  const propertyDepts =
-    application.externalData.getTaxReturnData.data.prefill.debt.debtLines.filter(
-      (line) => line.debtType.name === 'property',
-    )
-
-  console.log('application', propertyDepts)
   return (
     <FormScreen
       goToScreen={goToScreen}
